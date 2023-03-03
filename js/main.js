710112cfc7ab5d08e9b24eaaba1879dfbc32a79e
Iulia Koval <y-koval01@mail.ru>
2023-03-03 10:39:47 +0500

    добавляет две новые функции

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function getRandomInt (from, to) {
+    let fromTemp = from;
+    let toFrom = to;
+    if (from < 0 || to < 0) {
+        throw new RangeError ('Числа в диапазоне должны быть положительными'); 
+    }
+    if (typeof from === 'number' || typeof to === 'string') {
+        if (from === to) {
+            return from;
+        }
+        if (from > to) {
+            fromTemp = to;
+            toFrom = from;
+        }
+        return Math.round(Math.random() * (toFrom = fromTemp) + fromTemp);
+    }
+}
+console.log(getRandomInt([0,1,2,3], 9));
+
+const isCorrectLength = (str, maxLength) => {
+    if (typeof str !== 'string') {
+        throw new RangeError('Значение str должно быть строкой');
+    }
+    return str.length <= maxLength;
+};
+console.log(isCorrectLength([], 3));
