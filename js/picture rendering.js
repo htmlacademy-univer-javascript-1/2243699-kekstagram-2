0b3bd37bb97b394341f89a304355b724994db156
Iulia Koval <y-koval01@mail.ru>
2023-04-17 12:44:44 +0500

    Отрисуй меня часть 1 2

--- js/picture rendering.js
+++ js/picture rendering.js
@@ -1,15 +1,15 @@
 import {descriptions} from './data.js';
 
-const userPhotos = document.querySelector('#picture');
-const photoConteiner = document.querySelector('.picture');
+const userPhotosTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const photoConteiner = document.querySelector('.pictures');
+const userPhotos = descriptions();
 const photoListFragment = document.createDocumentFragment();
 
 userPhotos.forEach(({url, likes, comments}) => {
-    const photoElement = userPhotos.cloneNode(true);
+    const photoElement = userPhotosTemplate.cloneNode(true);
     photoElement.querySelector('.picture__img').src = url;
     photoElement.querySelector('.picture__likes').textContent = likes;
-    photoElement.querySelector('.picture__comments').textContent = comments;
-
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
     photoListFragment.append(photoElement);
 });
 
